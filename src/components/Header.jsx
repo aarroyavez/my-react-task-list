4e6014abc699eee14400644a6394d2a97058989a
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-13 22:39:56 -0500

    componente Header terminado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import styles from "./Header.module.css";
+import { AiOutlinePlusSquare} from "react-icons/ai"; 
+import todoapp from "../assets/todoapp.jpg";
 function Header(){
     return (
-     <header>
-        <p>header</p>
+     <header className={styles.header}>
+        <img src={todoapp} width={160} />
+    
+    <form className={styles.FormNuevaTarea}>
+        <input placeholder="Agregar una nueva tarea" type="text" />
+        <button>
+            
+            <AiOutlinePlusSquare size={25} />
+            </button>
+    </form>
+
     </header>
     )
 }
