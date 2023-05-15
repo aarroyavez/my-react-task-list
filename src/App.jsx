7d39f2c3d0cbc00b16f9ddeca6a5d2e4092e52d8
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-15 11:02:55 -0500

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Header from "./components/Header"
 // import Task from "./components/Task"
 import TaskList from "./components/TaskList"
+import { useState } from "react";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  function addTask(taskTitle){
+    setTaskList([
+      ...taskList,{
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false
+      }
+    ]);
+  }
   return (
     <>
-      <Header />
+      <Header onAddTask={addTask} />
       <TaskList />
       
     </>
