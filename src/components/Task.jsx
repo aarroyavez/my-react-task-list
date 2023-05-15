a7a374321e49b3840fd0f536803a7f186c6de1fa
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-15 09:42:15 -0500

    se crea lista de tareas sin funcionalidades aún, faltan hooks para que sea dinámica. Tres componentes creados: Header, Task y TaskList

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,16 @@
 import styles from "./Task.module.css";
-
+import {VscTrash} from "react-icons/Vsc";
 function Task() {
     return (
         <div className={styles.task}>
             <button className={styles.checkContainer}>
             <div />
+            </button>
+
+            <p>Ponerme al día con las clases de ADA School</p>
+            
+            <button className={styles.deleteButton}>
+                <VscTrash size={20} />
             </button>
         </div>
     )
