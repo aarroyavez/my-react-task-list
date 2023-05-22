ddc531568084eba61822311de01d7576649e8581
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-22 15:39:00 -0500

    prueba bien

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,85 +2,83 @@ import styles from "./Header.module.css";
 import { BsPlus} from "react-icons/bs"; 
 import todoapp from "../assets/todoapp.jpg";
 import { useState } from "react";
-import { registerUser } from "./services/registerUser";
 
-function Header(){
-    // const onSubmit = (data) => {
-    //     onSubmit(data);
-    // };
-
+function Header({onAddTask}){
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [formValidation, setFormValidation]=useState ({
-        title: undefined,
-        description: undefined,
-    });
+    const [formValidation, setFormValidation]=useState({error: false, errorMessage:""});
+    // const [error, setError] = useState("");
 
-    const handleSubmit = (event) => {
+    function handleSubmit (event){
         event.preventDefault();
-    };
 
-    const handleTitleChange=(event) => {
-        const value=event.target.value;
+        const error = title.length < 3;
+        setFormValidation({error: error, errorMessage: error ? "La tarea debe contener por lo menos tres(3) caracteres":""});
 
-        setFormValidation({
-            ...formValidation,
-            title: value < 3 ? "La tarea debe contener al menos 3 caracteres":"",
-        })
+        // if (title.length < 3) {
+        //     setError("La tarea debe contener por lo menos tres(3) caracteres");
+        //     return;
+        // }
 
-        setTitle(value);
-    };
-        
-        // onAddTask(title, description);
-        // setTitle("");
-        // setDescription("");
+        if (!error) {        
+        onAddTask(title, description);
+        setTitle("");
+        setDescription("");
+        }
         // setError;
+    };
+
+    function onKeyDown(event) {
+        if (event.key === "Enter"){
+            handleSubmit(event);
+        }
+    }
+
+    function onChangeTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function onChangeDescription(event) {
+        setDescription(event.target.value);
+    }
+
+    return (
+     <header className={styles.header}>
+        <img src={todoapp} width={160} />
     
-    
+    <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input 
+            className={styles.inputTitle}
+            placeholder="Agregar una nueva tarea" 
+            type="text" 
+            value={title} 
+            onChange={onChangeTitle} 
+        />
 
+        <input
+            className={styles.inputDescription}
+            placeholder="Agregar una descripción de tarea"
+            type="text"
+            value={description}
+            onChange={onChangeDescription}
+            onKeyDown={onKeyDown}
+        />
 
-
-    // function onKeyDown(event) {
-    //     if (event.value === "Enter"){
-    //         handleSubmit(event);
-    //     }
-    // };
-
-    const isFormValid = Object.keys(formValidation).every(
-        (key) => formValidation[key] === ""
-        );
-
-        return (
-            <header className={styles.header}>
-            <img src={todoapp} width={160} />
-            <form onSubmit={handleSubmit}
-            className={styles.newTaskForm}>
-                    <input 
-                        placeholder="Agregar una nueva tarea" 
-                        type="text" 
-                        value = {title}
-                        onChange={handleTitleChange}
-                        />
-                        {formValidation.title && (
-                            <span style={{color:"red"}}>{formValidation.title}</span>
-                    )}
-                    
-                    <textarea
-                        className={styles.newTaskForm}
-                        placeholder="Agregar una descripción de tarea"
-                        type="text"
-                        value={description}
-                        
-                        // onKeyDown={onKeyDown}
-                    />
-                    {formValidation.description && (
-                        <span style={{color: "red"}}>{formValidation.description}</span>
-                    )}
-        <button disabled={!isFormValid}><BsPlus size={25} /></button>
+        {formValidation.error ? (
+            <span className={styles.error}>{formValidation.errorMessage}</span>
+        ): null}
+        {/* {error && <p className={styles.error}>{error}</p>} */}
+        
+        <button>
+            <BsPlus size={25} 
+            />
+        </button>
     </form>
     </header>
-    );
+    )
 }
 
-
 export default Header;
+
+
+
